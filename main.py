5674a73e85ecb08f51d29e2bd06207d4d91b5cc5
repl.it user <replituser@example.com>
2021-02-03 19:23:33 +0000

    complete

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from unittest import main
 
 
 print(add_time("11:06 PM", "2:02"))
+print(add_time("8:16 PM", "466:02"))
+print(add_time("11:59 PM", "24:05"))
+
+# returns 1:08 AM
+
+# 6:18 AM, Monday (20 days later)
+
+# "12:04 AM (2 days later)"
 
 
 # Run unit tests automatically
